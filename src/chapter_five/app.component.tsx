51244f8cd0ee5ce5b2b27694f51113bc30988194
YuanGang26013 <262013@qq.com>
2020-01-16 10:37:23 +0800

    启用Redux调试工具

--- src/chapter_five/app.component.tsx
+++ src/chapter_five/app.component.tsx
@@ -3,12 +3,14 @@
  */
 import * as React from 'react'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 
 import IndexComponent from './index.component';
 import counter from './reducers';
 
-const store = createStore(counter);
+const store = createStore(counter,compose(
+    window && window[ 'devToolsExtension' ] ? window[ 'devToolsExtension' ]() : f => f
+));
 
 class AppComponent extends React.Component<any, any> {
     render() {
